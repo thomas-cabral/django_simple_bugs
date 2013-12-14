d9e426ac0b911c3dbb2817216bef0ea7284b3cba
Thomas Cabral <thethc96@yahoo.com>
2013-12-14 14:15:01 -0500

    added feature request and slugs to all models

--- simple_bugs/models.py
+++ simple_bugs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 # Create your models here.
 
 
@@ -7,6 +8,7 @@ class Requirement(models.Model):
     #data fields
     title = models.CharField(max_length=55)
     detail = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
     #date fields
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -16,6 +18,11 @@ class Requirement(models.Model):
     def __unicode__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(Requirement, self).save(*args, **kwargs)
+
     def get_absolute_url(self):
         return '/requirement/%i/' % self.id
 
@@ -24,6 +31,7 @@ class Bug(models.Model):
     #data fields
     title = models.CharField(max_length=55)
     detail = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
     #date fields
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -34,5 +42,31 @@ class Bug(models.Model):
     def __unicode__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(Bug, self).save(*args, **kwargs)
+
     def get_absolute_url(self):
         return '/bugs/%i/' % self.id
+
+
+class FeatureRequest(models.Model):
+    #data fields
+    title = models.CharField(max_length=55)
+    detail = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    #date fields
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    #relationships
+    user = models.ForeignKey(User)
+    requirement = models.ForeignKey(Requirement, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(FeatureRequest, self).save(*args, **kwargs)
