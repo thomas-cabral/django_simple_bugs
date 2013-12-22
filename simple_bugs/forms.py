a2c01a1ebd6e530b8fbedb35b9e35e9e071b51a4
Thomas Cabral <thethc96@yahoo.com>
2013-12-21 23:47:12 -0500

    chosen updates

--- simple_bugs/forms.py
+++ simple_bugs/forms.py
@@ -16,3 +16,6 @@ class RequirementForm(forms.ModelForm):
     class Meta:
         model = Requirement
         fields = ('title', 'detail', 'working_on')
+        widgets = {
+            'working_on': forms.SelectMultiple(attrs={'data-placeholder': 'Who is working on this?'})
+        }
