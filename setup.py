d5c330ebb0f06ca9c5eea8479bb0743cbf395fec
Thomas Cabral <thethc96@yahoo.com>
2014-05-04 23:22:29 -0400

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
     from distutils.core import setup
 
 #version = simple_bugs.__version__
-version = '0.1.2'
+version = '0.1.4'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
